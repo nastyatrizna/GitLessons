a7921b0622d0ce3b8adef6367bd1e633f5e512f3
Тризна Анастасия Александровна <anastasiya.trizna@service-it.by>
2025-02-13 12:01:59 +0300

    First commit. git lesson

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Фамилия, Имя: Тризна Анастасия");
+        System.out.println("Дата и время получения задания: 06.02.2025 23:04 ");
+    }
+}
