4502cd41cee04709e34f91d1fa5e001772050028
anastasiya.trizna <anastasia.trizna2512@gmail.com>
2025-02-14 09:55:50 +0300

    First task 2.0

--- src/by/homework/lessons/task1/Main.java
+++ src/by/homework/lessons/task1/Main.java
@@ -1,4 +1,8 @@
 package by.homework.lessons.task1;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Фамилия, Имя: Тризна Анастасия");
+        System.out.println("Дата и время получения задания: 06.02.2025 23:04 ");
+    }
 }
